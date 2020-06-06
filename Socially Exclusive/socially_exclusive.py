9c46d650358674d1fd70494e26fa605c57940241
Bitten Tech <anshbhawnani@gmail.com>
2020-06-06 21:13:44 +0530

    2nd wave updated

--- Socially Exclusive/socially_exclusive.py
+++
@@ -1,9 +0,0 @@
-import random
-from flag import flag
-
-key = "This needs to be private lol"
-
-for i in range(len(key)):
-    ciphertext = ""
-    for j in range(len(flag)):
-        ciphertext += chr(ord(flag[j]) ^ ord(key[i]));
